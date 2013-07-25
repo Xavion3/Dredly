f671c8ad5d97f3a2258e199eb4888f4cd3f1cacc
Xavion <Xavion333@gmail.com>
2013-07-25 14:04:51 +1000

    Modify main to work with new parser code.

--- main.py
+++ main.py
@@ -22,23 +22,33 @@ def makeZip(path):
 			print root, filename
 	zf.close()
 
-def parseFolder(path, tmp_path = os.path.join(os.path.curdir,'tmp')):
-	# TODO: (VH) Hook up to parsers
+def parseFolder(path, parser, tmp_path = os.path.join(os.path.curdir,'tmp')):
 	os.mkdir(tmp_path)
+	# TODO: (M) Make it generate foldres based on content instead.
 	os.mkdir(os.path.join(tmp_path,'mod'))
 	os.mkdir(os.path.join(tmp_path,'sprites'))
 	os.mkdir(os.path.join(tmp_path,'items'))
 	for root, dirs, files in os.walk(path):
 		for filename in files:
 			if filename.split('.')[-1] in DREDLY_EXTENSIONS: # If it's a dredly file then parse
-				pass#parse(os.path.join(root, filename), tmp_path)
+				f = open(os.path.join(root, filename), 'r')
+				parser.parseFile(f)
 			else: # Else just copy
 				print filename
 				print root
 				shutil.copy(os.path.join(root,filename), # The file to be copied
 					os.path.join(tmp_path,root.split(path)[1].partition(os.sep)[2],filename)) # Gets the equivalent location
+	# Now that you've parsed everything make the xml
+	parser.createXML()
+	for f in parser.xml:
+		ET.ElementTree(f[0]).write(os.path.join(tmp_path, f[1]))
 	makeZip(tmp_path)
 	shutil.rmtree(tmp_path)
 
 def main():
-	gui.main()
+	#gui.main()
+	# Parse the syntax.
+	f = open('./dredly/curSyntax.dredly', 'r')
+	Parser = parsers.Parser(f)
+	# Now run it on the test mod!
+	parseFolder('dredly/test_mod',Parser)
